fce7e9cc12a8e748b1c09f82dd399d076e73f666
e3kef <yutofu.6f@gmail.com>
2026-05-08 08:37:07 +0900

    add tusb_config.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "matrix.h"
+#include "keymap.h"
+#include "transport.h"
 
 
 int main()
 {
     stdio_init_all();
 
+    matrix_init();
+    transport_init();
+
+    bool previous_pressed = false;
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        transport_task();
+        matrix_task();
+
+        bool current_pressed = matrix_is_test_key_pressed();
+
+        if(current_pressed != previous_pressed){
+            if(current_pressed){
+                KeyboardReport report = keymap_make_report(true);
+                transport_send_report(report);
+            }else{
+                transport_send_release();
+            }
+
+            previous_pressed = current_pressed;
+        }
+
+        sleep_ms(3);
     }
 }
--- src/usb_hid.cpp
+++ src/usb_hid.cpp
@@ -5,5 +5,13 @@
 
 void usb_hid_init(){
     board_init();
-    tusb_init();
+    tusb_init(BOARD_TUD_RHPORT, &dev_init);
+}
+
+void usb_hid_task(){
+    tud_task();
+}
+
+bool usb_hid_ready(){
+    return tud_mounted() && tud_hid_ready();
 }
